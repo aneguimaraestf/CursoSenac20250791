b7fe74e0390364008b8c220587f42402221a9286
aneguimaraestf <253159413+aneguimaraestf@users.noreply.github.com>
2026-01-09 19:29:57 -0300

    resolucao

--- Aula19_Academia.py
+++ Aula19_Academia.py
@@ -8,14 +8,46 @@ with open("baseAcademia.json","r", encoding="utf-8") as arquivo:
 lista_alunos = dados["alunos"]
 
 # 1. Exibir a lista de alunos contendo Nome | Mensalidade R$ | Tipo de Plano | Frequência
-print("Lista de Alunos:")
-for aluno in lista_alunos:
-    nome = aluno["nome"]
-    mensalidade = aluno["mensalidade"]
-    tipo_plano = aluno["tipo_plano"]
-    frequencia = aluno["frequencia"]
-    print(f"- {nome} | Mensalidade: R$ {mensalidade:.2f} | Plano: {tipo_plano} | Frequência: {frequencia} vezes/semana")
 
-# 2. Exibir total de mensalidade por mês da academia
+print("Lista de Alunos:")
+print("Nome | Mensalidade R$ | Tipo de Plano | Frequência")
+for aluno in lista_alunos:
+    print(f"{aluno["nome"]} | {aluno["mensalidade"]} | {aluno["plano"]} | {aluno["frequencia_mensal"]}")
+
+
+print()
+print("--------- RESUMO ---------")
+# 2. Exibir total de receita da academia
+total_receita = 0
+for aluno in lista_alunos:
+    total_receita += aluno["mensalidade"]
+
+print(f"Receita Total Mensal: R$ {total_receita:.2f}")
+
 # 3. Exibir a média de idade dos alunos da academia
+total_idade = 0
+
+for aluno in lista_alunos:
+    total_idade += aluno["idade"]
+    
+media_idade = total_idade/len(lista_alunos)
+print(f"Idade Média dos Alunos: {int(round(media_idade,0))}")
+
 # 4. Frequência total por tipo de plano da academia
+total_frequencia = 0
+
+frequencia_por_plano = {
+
+}
+
+for aluno in lista_alunos:
+
+    if aluno["plano"] not in frequencia_por_plano:
+        frequencia_por_plano[aluno["plano"]] = 0
+
+    frequencia_por_plano[aluno["plano"]] += aluno["frequencia_mensal"]
+    total_frequencia += aluno["frequencia_mensal"]
+
+for plano in frequencia_por_plano:
+
+    print(f"{plano} - {frequencia_por_plano[plano]} - {round((frequencia_por_plano[plano]/total_frequencia)*100, 1)}%")
