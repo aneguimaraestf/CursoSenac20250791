e1bb3de02fd3f95f4726ca5fd698f6075fcb5a07
aneguimaraestf <253159413+aneguimaraestf@users.noreply.github.com>
2026-02-05 20:38:33 -0300

    Aula 5

---
+++ Pandas/Aula 4 e 5/Atividade.py
@@ -0,0 +1,85 @@
+# Missão 1: Configuração Inicial
+
+
+# 1. Importe pandas, numpy e datetime
+#pip install numpy
+import pandas as pd
+import numpy as np
+from datetime import datetime
+import locale
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+
+
+#2. Carregue o arquivo Excel com os dados da clínica
+dados_original = pd.read_excel("dados_clinica_simplificado.xlsx", engine="openpyxl")
+
+#Crie uma variável df_original para manter os dados originais
+#Crie uma cópia chamada dados para trabalhar
+dados = dados_original.copy()
+
+
+#3. Exiba as primeiras 10 linhas do DataFrame para entender a estrutura dos dados
+print(dados.head(10))
+
+
+#4. Verifique quantas linhas e colunas existem no tota
+print(
+    f"\nA quantidade de colunas é: {dados.shape[1]:,}".replace(",", ".") +
+    f"\nA quantidade de linhas é: {dados.shape[0]:,}".replace(",", ".")
+)
+
+
+#5. Identifique o tipo de dado de cada coluna
+print(f"\nOs tipos de dados das colunas, são: \n{dados.dtypes}")
+
+
+#6. Conte quantos valores nulos existem em cada coluna
+print(f"\nQuantidade de valores nulos: \n{dados.isnull().sum()}")
+
+
+#Missão 2: Padronização dos Nomes
+#1. Converta todos os nomes para string (caso ainda não estejam)
+dados['Nome'] = dados['Nome'].astype(str)
+
+
+#2. Remova espaços extras no início e fim de cada nome
+dados['Nome'] = dados['Nome'].str.strip()
+
+
+#3. Padronize os nomes para o formato "Primeira Letra Maiúscula" (ex: "Maria Silva")
+dados['Nome'] = dados['Nome'].str.title()
+
+
+#4. Verifique se a correção foi aplicada corretamente
+#print(dados.sample(10))
+
+
+#Missão 3: Correção das Idades
+#1. Substitua os valores vazios e "N/A" por NaN (valores ausentes)
+def corrigir_idade(valor):
+    try:
+        idade = int(valor)
+        return idade
+    except:
+        idade = pd.NA
+dados["Idade"] = dados["Idade"].replace(["", " ", "N/A"], pd.NA)
+
+dados["Idade"] = pd.to_numeric(dados["Idade"], errors="coerce")
+
+# Se a coluna for extremamente importante, faz sentido filtrar os dados ruins
+
+# dados_limpo = dados[dados["Idade"].notna()]
+
+mediana_idade = dados[dados["Idade"].notna()]["Idade"].median()
+
+dados.loc[dados["Idade"].isna(), "Idade"] = mediana_idade
+
+dados["Idade"] = dados["Idade"].astype("int64")
+
+dados["Idade"] = dados["Idade"].clip(lower=0, upper=120)
+
+# print(dados[(dados["Idade"] < 0) | (dados["Idade"] > 120)])
+
+
+# print(mediana_idade)
+print(dados["Sexo"].value_counts())
